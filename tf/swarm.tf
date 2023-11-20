6050834a63d51f31a28bffa483b87ab99b4a3224
Super User <root@tool.loc.ntcl.pl>
2023-11-20 12:26:01 +0000

    minor fixes

--- tf/swarm.tf
+++ tf/swarm.tf
@@ -1,10 +1,8 @@
-resource "proxmox_virtual_environment_vm" "swarm" {
-	name = "swarm-${count.index}"
-	description = "Swarm test worker${count.index}"
-	tags = ["terraform", "debian"]
-    vm_id = 500 + count.index
-
-	count = 3
+resource "proxmox_virtual_environment_vm" "docker" {
+	name = "docker0"
+	description = "Docker host 0"
+	tags = ["terraform", "debian", "docker"]
+    vm_id = 120
 
 	node_name = "hv01"
 	
@@ -15,19 +13,19 @@ resource "proxmox_virtual_environment_vm" "swarm" {
 	clone {
 		datastore_id = "local-lvm"
 		retries = 3
-		vm_id = 9000
+		vm_id = 8010
 	}
 
 	initialization {
-    ip_config {
-      ipv4 {
-        address = "10.0.5.${count.index}/16"
-        gateway = "10.0.0.1"
-      }
+		ip_config {
+		ipv4 {
+			address = "10.0.0.120/16"
+			gateway = "10.0.0.1"
+		}
     }
-    user_account {
-      username = "root"
-      keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8f2gLQOHg2Xnnf9EyZ1A8eL1nyCh0+aZpBxj9eRowF gkk@gkk-pc", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/zzGElglUoqeFr+siF/lulMl8kHGFwTMQcFhV3PWaH root@tool"]
-    }
+		user_account {
+			username = "debian"
+			keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8f2gLQOHg2Xnnf9EyZ1A8eL1nyCh0+aZpBxj9eRowF gkk@gkk-pc", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/zzGElglUoqeFr+siF/lulMl8kHGFwTMQcFhV3PWaH root@tool"]
+		}
 	}
 }
